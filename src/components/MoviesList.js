b6b938f3cf5bb3db8fb0b1a4f29d1ff8c7610f06
robcrockPI <robert@propellerindustries.com>
2020-11-11 11:18:25 -0300

    Add a filter to my React playground.

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const movies = [
+  { name: "36th Chamber"},
+  { name: "5 Deadly Venoms"},
+  { name: "Man of Iron"},
+]
 
 export function MoviesList() {
+  
+  const [filter, setFilter] = useState("");
 
-  const movies = [
-    { name: "Man of Iron"},
-    { name: "Man of Iron"},
-    { name: "Man of Iron"},
-    { name: "Man of Iron"},
-    { name: "Man of Iron"},
-  ]
   return (
     <div>
+      <label>
+        Filter:
+        <input 
+          onChange={ (e) => setFilter(e.target.value) }
+          value={ filter }
+        ></input>
+      </label>
       <ul>
         {/* This create the array of lis we want to render */}
-        { movies.map((movie, i) => <li key={i}>{ movie.name }</li>)}
+        { movies
+          .filter((movie) => { 
+            return (
+              movie.name.toLowerCase().includes(filter.toLowerCase())
+            )}
+          )
+          .map((movie, i) => { 
+            return (
+              <li key={i}>{ movie.name }</li>
+            )}
+          )
+        }
       </ul>
     </div>
   )
